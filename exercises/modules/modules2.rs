34f6683248797d5acdf49ed1b1d29c6d1439e5a3
Zhang Rundong <rundong.zhang350@gmail.com>
2023-10-13 21:04:05 +0800

    the forth commit.

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -8,13 +8,12 @@
 // hint.
 
 
-
-pub mod delicious_snacks {
+mod delicious_snacks {
     // TODO: Fix these use statements
     pub use self::fruits::PEAR as fruit;
     pub use self::veggies::CUCUMBER as veggie;
 
-    pub mod fruits {
+    mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
